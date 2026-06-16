e6ca54513188717084aa141194c5cc8bc0a0139d
Shivika Singh <shivikasingh@Shivikas-MacBook-Air.local>
2026-06-16 22:07:37 +0530

    New initial setup along with connecting Express server to PostgreSQL

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
-console.log("File is being executed");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend is running!");
+  res.send("Backend is running");
 });
 
 app.listen(5100, () => {
